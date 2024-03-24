cb3c98868dd08e79af37134323c3ba88fa98666b
SuvekaD <160578580+SuvekaD@users.noreply.github.com>
2024-03-24 12:54:00 +0530

    Update evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -1,26 +1,22 @@
+from joblib import load
 import pandas as pd
 from sklearn.metrics import accuracy_score
-from joblib import load
+from sklearn.preprocessing import StandardScaler
 
-def evaluation_pipeline(x_test_path, y_test_path, model_paths):
-    # Read data
-    X_test = pd.read_csv(x_test_path)
-    y_test = pd.read_csv(y_test_path)
+def evaluation_pipeline(x_test_path, y_test_path, model_path):
+    X = pd.read_csv(x_test_path)
+    y_target = pd.read_csv(y_test_path)
+    
+    numerical_columns = X.select_dtypes(exclude='object')
 
-    # Load scaler
-    scaler = load('scaling/scaler.pkl')
+    scaler = load('Scaling/scaler.pkl')
+    numerical_scaled_data = scaler.transform(numerical_columns)
+    numerical_scaled_data = pd.DataFrame(numerical_scaled_data, columns=numerical_columns.columns)
 
-    # Scale features
-    X_test_scaled = scaler.transform(X_test)
+    Features = pd.concat([numerical_scaled_data], axis=1)
 
-    # Load models and make predictions
-    predictions = []
-    scores = []
-    for model_path in model_paths:
-        model = load(model_path)
-        y_pred = model.predict(X_test_scaled)
-        predictions.append(y_pred)
-        score = accuracy_score(y_test, y_pred) * 100
-        scores.append(score)
+    model = load(model_path)
+    y_pred = model.predict(Features)
+    test_score = accuracy_score(y_target, y_pred) * 100
 
-    return predictions, scores
+    return y_pred, test_score
